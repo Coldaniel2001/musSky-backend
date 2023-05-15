eec2f198f8a945e17876d49bac0cf177d593c14d
Victor Jauregui <vicjaudom@hotmail.com>
2023-05-15 12:38:53 +0200

    getPlayListById done

--- src/routes/playlists.routes.js
+++ src/routes/playlists.routes.js
@@ -3,7 +3,7 @@ const app = express()
 
 const router = express.Router()
 
-const { createPlaylist, getAllPlaylists } = require("../controllers/playlistControllers")
+const { createPlaylist, getAllPlaylists, getPlaylistById } = require("../controllers/playlistControllers")
 
 const { getError } = require("../middlewares/error.middleware")
     
@@ -13,6 +13,7 @@ const { jwtCheck } = require("../middlewares/check-middlewares")
 
 router.post("/newplaylist", createPlaylist)
 router.get("/", getAllPlaylists)
+router.get("/id/:userId", getPlaylistById)
 
 
 
